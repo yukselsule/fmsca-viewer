2b5ecd977b31e237e8d272bd0ec3cff7cbeb4288
yukselsule <sulecantekin@gmail.com>
2024-08-01 19:17:42 +0300

    Filter context

---
+++ src/components/Filters.js
@@ -0,0 +1,55 @@
+import { TextField, Box, Typography } from "@mui/material";
+
+import { useFilters } from "../contexts/FilterContext";
+
+const labelMapping = {
+  created_dt: "Created Date",
+  data_source_modified_dt: "Modified Date",
+  entity_type: "Entity Type",
+  operating_status: "Operating Status",
+  legal_name: "Legal Name",
+  dba_name: "DBA Name",
+  physical_address: "Physical Address",
+  phone: "Phone",
+  usdot_number: "USDOT Number",
+  mc_mx_ff_number: "MC/MX/FF Number",
+  power_units: "Power Units",
+  out_of_service_date: "Out of Service Date",
+};
+
+function Filters() {
+  const { filters, handleFilterChange } = useFilters();
+
+  return (
+    <Box
+      className="filters"
+      sx={{
+        backgroundColor: "white",
+        padding: 2,
+        borderRadius: 2,
+        boxShadow: 1,
+        marginBottom: 3,
+      }}
+    >
+      <Typography variant="h6" gutterBottom sx={{ color: "#090909" }}>
+        Filters
+      </Typography>
+      <Box sx={{ display: "flex", flexWrap: "wrap", gap: 2 }}>
+        {Object.keys(filters).map((key) => (
+          <TextField
+            key={key}
+            name={key}
+            label={labelMapping[key] || key.replace(/_/g, " ")}
+            value={filters[key]}
+            onChange={handleFilterChange}
+            variant="outlined"
+            size="small"
+            sx={{ minWidth: { xs: "100%", sm: 200 }, flexGrow: 1 }}
+          />
+        ))}
+      </Box>
+    </Box>
+  );
+}
+
+export default Filters;
---
+++ src/contexts/FilterContext.js
@@ -0,0 +1,41 @@
+import { createContext, useContext, useState } from "react";
+
+const FiltersContext = createContext();
+
+function FiltersProvider({ children }) {
+  const [filters, setFilters] = useState({
+    created_dt: "",
+    data_source_modified_dt: "",
+    entity_type: "",
+    operating_status: "",
+    legal_name: "",
+    dba_name: "",
+    physical_address: "",
+    phone: "",
+    usdot_number: "",
+    mc_mx_ff_number: "",
+    power_units: "",
+    out_of_service_date: "",
+  });
+  const handleFilterChange = (event) => {
+    setFilters({
+      ...filters,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  return (
+    <FiltersContext.Provider
+      value={{ filters, setFilters, handleFilterChange }}
+    >
+      {children}
+    </FiltersContext.Provider>
+  );
+}
+
+function useFilters() {
+  const context = useContext(FiltersContext);
+  return context;
+}
+
+export { FiltersProvider, useFilters };
